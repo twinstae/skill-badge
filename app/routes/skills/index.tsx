1da98317dd2091b3f1970a7e4bc8e9669928c6c7
Taehee Kim <rabolution@gmail.com>
2022-09-10 17:27:59 +0900

    feat: 간단하고 무식한 역량 검색

--- app/routes/skills/index.tsx
+++ app/routes/skills/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import CenterCardLayout from '~/components/CenterCardLayout';
@@ -20,20 +20,35 @@ export const loader = async () => {
 
 export default function SkillListPage() {
   const { skills } = useLoaderData() as LoaderData;
+  const [search, setSearch] = useState('');
+  const lowerSearch = search.toLowerCase();
+
+  const filteredSkills = skills.filter((skill) =>
+    skill.slug.includes(lowerSearch)
+    || skill.title.toLowerCase().includes(lowerSearch)
+  );
   return (
     <CenterCardLayout>
       <h1>역량 목록</h1>
       <Link className="btn btn-sm btn-primary w-full" to="/skills/admin/new">
         새 역량 만들기
       </Link>
+      <input
+        type="text"
+        className="input input-bordered mt-2 w-full"
+        value={search}
+        placeholder="ex) database, 데이터베이스, react, 오픈 소스"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <ul className="p-2 m-2 menu">
-        {skills.slice(0,10).map((skill) => (
-          <li key={skill.slug}>
-            <Link to={'/skills/' + skill.slug} className="p-2">
-              {skill.title}
-            </Link>
-          </li>
-        ))}
+        {filteredSkills.slice(0,10).map((skill) => (
+            <li key={skill.slug}>
+              <Link to={'/skills/' + skill.slug} className="p-2">
+                {skill.title} <span className="text-sm text-primary">{skill.slug}</span>
+              </Link>
+            </li>
+          ))}
+        {filteredSkills.length > 10 && <li className="text-2xl text-center">...{filteredSkills.length - 10} 개</li>}
       </ul>
     </CenterCardLayout>
   );
