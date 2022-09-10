4d73a850a6f5b8893c32b1fbe246883c05938a9f
Taehee Kim <rabolution@gmail.com>
2022-09-10 10:41:08 +0900

    fix: SkillList 배경과 테두리 style

--- app/components/SkillList.tsx
+++ app/components/SkillList.tsx
@@ -7,7 +7,11 @@ const SkillList = createOptionalDataList<string>({
   className: 'grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3',
   Item({ data: slug }) {
     return (
-      <LinkWithTooltip to={'/skills/' + slug} tooltip={'/skills/' + slug} className="p-2 bg-slate-100 m-1 rounded-md">
+      <LinkWithTooltip
+        to={'/skills/' + slug}
+        tooltip={'/skills/' + slug}
+        className="p-1 m-1 rounded-md bg-quote border-2 border-transparent focus:border-primary-focus"
+      >
         {slug}
       </LinkWithTooltip>
     );
