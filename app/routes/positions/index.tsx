da35c301c273c0516bc92000ee6133c2ad6f9a49
Taehee Kim <rabolution@gmail.com>
2022-09-10 17:28:31 +0900

    feat: Position 선택 페이지를 layout과 flex로 변경

--- app/routes/positions/index.tsx
+++ app/routes/positions/index.tsx
@@ -1,9 +1,10 @@
+import CenterCardLayout from '~/components/CenterCardLayout';
 import { Link } from '~/Link';
 
 function PositionLink({ slug, title }: { slug: string; title: string }) {
   return (
     <Link
-      className="w-1/2 h-full m-1 btn btn-primary flex items-center justify-center"
+      className="flex-1 h-full m-1 btn btn-primary flex items-center justify-center"
       to={'/positions/' + slug}
     >
       <h2 className="text-4xl text-white">{title}</h2>
@@ -13,14 +14,12 @@ function PositionLink({ slug, title }: { slug: string; title: string }) {
 
 export default function PositionPage() {
   return (
-    <section className="w-full flex justify-center">
-      <div className="w-full max-w-xl h-full flex flex-col items-center">
-        <h1>어떤 직군의 공고를 보실래요?</h1>
-        <div className="w-full h-96 mt-4 flex items-center">
-          <PositionLink slug="frontend" title="프런트엔드" />
-          <PositionLink slug="backend" title="백엔드" />
-        </div>
+    <CenterCardLayout>
+      <h1>어떤 직군의 공고를 보실래요?</h1>
+      <div className="w-full h-96 mt-4 flex items-stretch">
+        <PositionLink slug="frontend" title="프런트엔드" />
+        <PositionLink slug="backend" title="백엔드" />
       </div>
-    </section>
+    </CenterCardLayout>
   );
 }
