92c04a1083d1062990f4b87dabc79f1605a4b083
Taehee Kim <rabolution@gmail.com>
2022-09-14 14:28:57 +0900

    test: BadgeRepo

---
+++ app/models/badges/repo.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest';
+import { FakeBadgeRepo } from './fakeRepo';
+import rawFakeBadgeList from './fakeBadgeList.json';
+import { z } from 'zod';
+import { badgeSchema } from './schema';
+
+describe('BadgeRepo', () => {
+	const repo = FakeBadgeRepo(z.array(badgeSchema).parse(rawFakeBadgeList));
+
+	const newBadgeSlug = 'database-fake-for-test';
+	it('새로운 배지를 만들 수 있다', async () => {
+		const oldCount = await repo.getBadgeList().then(list => list.length);
+
+		const newBadge = {
+			slug: newBadgeSlug,
+			title: 'database 테스트용 배지',
+			skillSlugs: ['database'],
+			pieces: [],
+			icon: 'commandLine' as const
+		};
+		await repo.createBadge(newBadge);
+
+		await expect(repo.getBadgeList().then(list => list.length))
+			.resolves
+			.toBe(oldCount+1)
+
+		await expect(repo.getBadgeWithPieces(newBadgeSlug))
+			.resolves
+			.toStrictEqual(newBadge)
+	});
+});
