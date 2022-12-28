c3f27bacf98b5771d6b60f45e334d9d0f9342751
Taehee Kim <rabolution@gmail.com>
2022-12-28 12:25:52 +0900

    fix: height fit content

--- app/components/CenterCardLayout.tsx
+++ app/components/CenterCardLayout.tsx
@@ -8,7 +8,7 @@ export default function CenterCardLayout({ children, className }: {
 }) {
 	return (
 		<div className="flex flex-row justify-center min-h-screen">
-			<div className="static flex flex-col md:rounded-xl m-0 md:m-2 shadow-2xl md:border-2 max-w-xl w-full">
+			<div className="static flex flex-col sm:rounded-xl m-0 md:m-2 shadow-2xl md:border-2 max-w-xl w-full h-fit max-h-screen overflow-y-auto">
 				<NavigationBar />
 				<main id="main" className={clsx('h-full w-full p-4', className)}>
 					{children}
